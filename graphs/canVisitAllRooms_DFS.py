1973049d1f3854e467151adbdcb08ea7074bca3a
Exquisition <l.zhou@mail.utoronto.ca>
2019-07-21 13:07:14 -0400

    easy dfs with stack

---
+++ graphs/canVisitAllRooms_DFS.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
+        
+        seen = [False] * len(rooms)
+        
+        seen[0] = True
+        stack = [0]
+        
+        while stack:  
+            node = stack.pop() 
+            for nei in rooms[node]:
+                if not seen[nei]: 
+                    seen[nei] = True 
+                    stack.append(nei) 
+                    
+        return all(seen)
+        
