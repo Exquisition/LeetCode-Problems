01300d1a39e705d33913ec01921192b29c605261
Exquisition <l.zhou@mail.utoronto.ca>
2019-07-22 22:57:37 -0400

    bst from preorder using stack

---
+++ trees/bstFromPreorder.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        
+        root = TreeNode(preorder[0])
+        stack = [root]
+        
+        for value in preorder[1:]:
+            
+            if value < stack[-1].val:
+                stack[-1].left = TreeNode(value)
+                stack.append(stack[-1].left)
+            else:
+                while stack and stack[-1].val < value:
+                    last = stack.pop()
+                last.right = TreeNode(value)
+                stack.append(last.right)
+                
+        return root
+        
